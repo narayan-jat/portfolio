33489f1b853b66d4000e3ab6db05565f3a99f15f
Narayan Jat <narayan.jat>
2024-07-02 12:20:28 +0530

    add skills

--- front-end/src/components/Skills.js
+++ front-end/src/components/Skills.js
@@ -14,40 +14,93 @@ import english from "../media/lang-en.png";
 import hindi from "../media/lang-en.png";
 import "../styles/Skill.css";
 
-const programmingImages = [mongoDb, reactJs, python, html5, expressJS, nodeJS, 
-    javascript, next, cssImage]
+const programmingSkills= [ {
+    name: "Mongo DB",
+    image: mongoDb
+},
+{
+    name: "React JS",
+    image: reactJs
+},
+{
+    name: "Python",
+    image: python
+},
+{
+    name: "Mongo DB",
+    image: mongoDb
+},
+{
+    name: "HTML 5",
+    image: html5
+},
+{
+    name: "Express",
+    image: expressJS
+},
+{
+    name: "NodeJs",
+    image: nodeJS
+},
+{
+    name: "Javacript",
+    image: javascript
+},
+{
+    name: "Next",
+    image: next
+},
+{
+    name: "CSS",
+    image: cssImage
+},
+];
 
 const Skills = () => {
-
-    return (
-        <div id="skills" className="skill-container">
-            <h2 className="skill-title">Programming Skills</h2>
-            <div className="skills">
-               {programmingImages.map((image, index) => (
-                <div key={index} className="d-flex flex-column">
-                     <img className="skill-image" src={image} alt="not found"></img>
-                     <span>React Js</span>
-                </div>
-               ))}
+  return (
+    <div style={{ backgroundColor: "black", color: "white", width: "100%"}}>
+      <h2
+        style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          color: "green",
+          padding: "50px 0",
+        }}
+      >
+        Skills
+      </h2>
+      <div id="skills" className="skill-container">
+        <h2 className="skill-title">Programming Skills</h2>
+        <div className="skills">
+          {programmingSkills.map((skill, index) => (
+            <div key={index} className="skill-image-box d-flex flex-column align-items-center">
+              <img className="skill-image" src={skill.image} alt="not found"></img>
+              <span>{skill.name}</span>
             </div>
-            <h2 className="skill-title">Programming Skills</h2>
-            <div className="skills">
-               {programmingImages.map((image, index) => (
-                <div key={index}>
-                     <img className="skill-image" src={image} alt="not found"></img>
-                </div>
-               ))}
-            </div>
-            <h2 className="skill-title">Programming Skills</h2>
-            <div className="skills">
-               {programmingImages.map((image, index) => (
-                <div key={index}>
-                     <img className="skill-image" src={image} alt="not found"></img>
-                </div>
-               ))}
-            </div>
+          ))}
         </div>
-    )
-}
+        <h2 className="skill-title">Tools and Other Skills</h2>
+        <div className="skills">
+          {programmingSkills.map((skill, index) => (
+            <div key={index} className=" skill-image-box d-flex flex-column align-items-center">
+              <img className="skill-image" src={skill.image} alt="not found"></img>
+              <span>{skill.name}</span>
+            </div>
+          ))}
+        </div>
+        <h2 className="skill-title">Programming Skills</h2>
+        <div className="skills">
+          {programmingSkills.map((skill, index) => (
+            <div key={index} className=" skill-image-box d-flex flex-column align-items-center">
+              <img className="skill-image" src={skill.image} alt="not found"></img>
+              <span>{skill.name}</span>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Skills;

053aac5524e1108264f0179db82963638d4712d7
pankajyadav8523 <py9621008569@gmail.com>
2024-07-07 19:19:56 +0530

    Changing name of Portfolio

--- front-end/src/components/Projects.js
+++ front-end/src/components/Projects.js
@@ -3,21 +3,21 @@ import projectImage from "../media/projects-default.jpg";
 import strucutre from "../media/structure.png";
 import cleaning from "../media/cleaning.png";
 import tic from "../media/tic.png";
-import dropout from "../media/dropout.avif"
+import dropout from "../media/dropout.avif";
 
 import "../styles/Projects.css";
 import { Link } from "react-router-dom";
 
 const projects = [
   {
-    name: "Portfolio",
+    name: "Nice Portfolio",
     details:
       "This portfolio project showcases a collection of my work and achievements, demonstrating my skills and expertise in various domains. It includes detailed descriptions of individual projects, highlighting my proficiency in programming, data analysis, machine learning, and software development. Each project is accompanied by source code, visualizations.",
     imageUrl: projectImage,
     sourceUrl: "https://github.com/narayan-jat/portfolio",
     liveUrl: "https://github.com/narayan-jat/portfolio",
     techStack: "React, Node",
-    techField: "Web Development"
+    techField: "Web Development",
   },
   {
     name: "Data Structures Repository",
@@ -27,18 +27,18 @@ const projects = [
     sourceUrl: "https://github.com/narayan-jat/Data-structures",
     liveUrl: "https://github.com/narayan-jat/Data-structures",
     techStack: "Python",
-    techField: "Data strucuture and Algorithms"
+    techField: "Data strucuture and Algorithms",
   },
   {
-  name: "Text Cleaner",
-  details:
-    "This text cleaner tool in Java designed to clean text files in XML, TXT, and JSON formats based on custom-defined rules. This tool efficiently processes and sanitizes text, ensuring consistency and accuracy by applying specific cleaning criteria tailored to various file types. It is ideal for preparing data for further analysis or use in applications where text quality is crucial.",
-  imageUrl: cleaning,
-  sourceUrl: "https://github.com/narayan-jat/Text-Cleaning",
-  liveUrl: "https://github.com/narayan-jat/Text-Cleaning",
-  techStack: "Java",
-  techField: "Data cleaning"
-},
+    name: "Text Cleaner",
+    details:
+      "This text cleaner tool in Java designed to clean text files in XML, TXT, and JSON formats based on custom-defined rules. This tool efficiently processes and sanitizes text, ensuring consistency and accuracy by applying specific cleaning criteria tailored to various file types. It is ideal for preparing data for further analysis or use in applications where text quality is crucial.",
+    imageUrl: cleaning,
+    sourceUrl: "https://github.com/narayan-jat/Text-Cleaning",
+    liveUrl: "https://github.com/narayan-jat/Text-Cleaning",
+    techStack: "Java",
+    techField: "Data cleaning",
+  },
   {
     name: "Tic Tac Toe Game",
     details:
@@ -47,16 +47,17 @@ const projects = [
     sourceUrl: "https://github.com/narayan-jat/game-project",
     liveUrl: "https://github.com/narayan-jat/game-project",
     techStack: "Flask, MySQL, HTML, CSS, Javascript",
-    techField: "Machine Learning"
+    techField: "Machine Learning",
   },
   {
     name: "Dropout Analysis Portal",
-    details: "Students are dropping out their studies for the several reasons and we don't have anything to analyse all the dropout rate in a particular field and work on it. This portal will help in finding the main factors in a particular region on which a proper focus is required. It will maximize the benefits of student community and provides many predefined parameters to invets into.",
+    details:
+      "Students are dropping out their studies for the several reasons and we don't have anything to analyse all the dropout rate in a particular field and work on it. This portal will help in finding the main factors in a particular region on which a proper focus is required. It will maximize the benefits of student community and provides many predefined parameters to invets into.",
     imageUrl: dropout,
     sourceUrl: "https://github.com/narayan-jat/SUStar-HackCBS",
     liveUrl: "https://github.com/narayan-jat/SUStar-HackCBS",
     techStack: "Flask, MySQL, HTML, CSS, Javascript",
-    techField: "Web development"
+    techField: "Web development",
   },
 ];
 function Projects() {
@@ -67,7 +68,7 @@ function Projects() {
         <Carousel
           data-bs-theme="dark"
           className="project-carousel"
-          interval= {null}
+          interval={null}
           keyboard={true}
           touch={true}
         >
@@ -86,8 +87,7 @@ function Projects() {
                   />
                 </Link>
                 <Carousel.Caption id="project-texts">
-                  <h5 style={{fontWeight: 700,
-                  }}>{project.name}</h5>
+                  <h5 style={{ fontWeight: 700 }}>{project.name}</h5>
                   <Link
                     to={project.sourceUrl}
                     target="_blank"
@@ -96,8 +96,13 @@ function Projects() {
                   >
                     <p className="project-details">{project.details}</p>
                   </Link>
-                  <h6 style={{fontWeight: "700"}}>{project.techField}</h6>
-                  <h6 style={{textAlign:"left", padding: "5px 0"}}>Tech Stack: <span style={{fontWeight: "700"}}>{project.techStack}</span></h6>
+                  <h6 style={{ fontWeight: "700" }}>{project.techField}</h6>
+                  <h6 style={{ textAlign: "left", padding: "5px 0" }}>
+                    Tech Stack:{" "}
+                    <span style={{ fontWeight: "700" }}>
+                      {project.techStack}
+                    </span>
+                  </h6>
                   <Link
                     to={project.liveUrl}
                     target="_blank"
@@ -107,7 +112,7 @@ function Projects() {
                   </Link>
                 </Carousel.Caption>
               </div>
-          </Carousel.Item>
+            </Carousel.Item>
           ))}
         </Carousel>
       </div>
